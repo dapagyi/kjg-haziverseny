0705562b5770e9465cf830d735bae0e1c977ef94
Apagyi Dávid <apagyi.david@gmail.com>
2019-01-11 01:37:36 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,61 @@
+# KJG háziverseny
+A hatodik osztályos nyílt matematika háziversenyre készült.
+
+## Működés
+### Kérdések beállítása (*questions.txt* fájlban):
+- egy kérdés egy sor (új sort a \<br> beszúrásával lehet kezdeni)
+- Feladat; pontszám; megoldás (például, négy pontért:  ```Mennyi 1-1?; 4; 0```)
+- LaTeX beszúrható
+- pontosvessző nem használható a feladat szövegében
+
+### Indítás
+```
+$ npm install
+$ node app
+```
+A kliensek nyissák meg a kiszolgáló helyi ip-jét böngészőben. 
+### Admin
+[Adminfelület](http://localhost/szupertitkos) funkciók:
+- új kérdés küldése
+- időkorlát beállítása
+- kép küldése a public mappából
+- tippek nyomonkövetése
+
+### Alapértelmezett pontozás
+Hibás megoldásért nem jár pontlevonás, hanem csökken a feladat pontértéke. Egy alá nem csökken a feladat pontértéke, tehát bárhanyadjára találja el a felhasználó a megoldást, kap legalább egy pontot.
+
+A pontszám [grafikonon](http://localhost/stats) nyomonkövethető, kivetíthető.
+
+## Képernyőképek
+![Felhasználó](public/screenshots/user.JPG)
+![Statisztika](public/screenshots/stats.JPG)
+![Admin felület](public/screenshots/admin.JPG)
+
+
+<br><br><br><br><br>
+
+## Tennivalók, ötletek, fejlesztési lehetőségek
+#### Általános
+- könnyen felhasználhatóvá alakítani, a használatát jól dokumentálni és elérhetővé tenni másoknak (csalások, kiskapuk?)
+#### Felhasználó
+- kiküldött kép lekérése frissítéskor
+#### Admin
+- bejelentkezés (mi alapján?)
+- barátságosabb felület
+- automatikus kérdésléptetés az idő lejárta (/ + néhány másodperc) után, checkbox
+- pontszám módosítása
+- log a böngésző konzoljába
+- adatok a verseny során
+#### Szerver
+- kérdések: időkorlát, kép opcionálisan beállítható (kérdéssor létrehozására és mentésére oldal? Ezeket az előre elkészített fájlokat később be lehetne tölteni. [külön mappa ezeknek, ahonnan választani lehet?])
+- fájl frissülésre automatikusan töltse újra a kérdéseket
+- debug elemek törlése
+- fejlett log (mappa, fájlok létrehozása)
+- program leállítása, log dátum-idő alapján való átmásolása
+- admin üzeneteket elkülönítve küldeni (külön tárolni az admin socketeket) (socket.io szoba?)
+
+<!--
+```javascript
+console.log("alma");
+```
+-->
---
+++ app.js
@@ -0,0 +1,180 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var fs = require("fs");
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.enable('trust proxy');
+
+var questions = [];
+var current = -1;
+function qload(){
+    questions = [];
+    fs.readFileSync("./questions.txt", "utf8").split("\r\n").forEach((elem)=>{
+        if(elem.includes(";")){
+        var data = elem.split("; ");
+        questions.push(data);
+        }
+    });
+    console.log("Kérdések:", questions);
+}
+qload();
+var logname = new Date().toISOString().replace(/:/g,'')./*replace(//g,'').*/slice(0,17);
+console.log(logname);
+function log(m, file = "log.txt"){
+    m = new Date().toLocaleTimeString() + " > " + m;
+    console.log(m);
+    if (!fs.existsSync('./data')) fs.mkdirSync('./data');
+    fs.appendFileSync("./data/"+logname+'-'+file, m + "\n","utf8");
+    if (file=="log.txt") io.emit("admin:log", m);
+}
+
+var sockets = [];
+var scores = {};
+var timeback = -1;
+
+io.on("connection", (socket) => {
+    var u;
+    socket.emit("welcome");
+    socket.on("welcome", (data)=>{
+        socket.user = data;
+        socket.user.sid = socket.id;
+        sockets.push(socket.user);
+        u = socket.user.name;
+        if (!scores.hasOwnProperty(u)) {
+            scores[u] = {value: 0, tips: []}
+            console.log(scores);
+        }
+        socket.emit("newquestion", (current == -1) ? "A verseny kezdete után itt fognak megjelenni a kérdések." : questions[current][0]);
+        log(u + " csatlakozott.");
+        refreshAdmin();
+    });
+
+    socket.on("admin:refresh", ()=>{
+        refreshAdmin();
+    });
+
+    socket.on("admin:sendimg", (url)=>{
+        log("Képlink érkezett: " + url);
+        io.emit("img", url);
+    });
+
+    socket.on("admin:sendpm", (data)=>{
+        log("Privát üzenet: " + JSON.stringify(data));
+        io.emit("pm", data);
+    });
+
+    var tin = null;
+    socket.on("admin:starttimer", (sec) => {
+        if (tin!=null) clearInterval(tin);
+        timeback = sec;
+        tin = setInterval(function(){
+            if (timeback <= 0) {
+                io.sockets.emit('inttick', 0);
+                timeback = -1; 
+                clearInterval(tin);
+            }
+            else {
+                io.sockets.emit('inttick', timeback);
+                timeback--;
+            }
+        }, 1000);
+    });
+
+    socket.on("admin:modifyscore", data=>{
+        score(data.team, data.score);
+    });
+
+    socket.on("admin:newquestion", (n)=>{
+        if ((current + parseInt(n)) >= 0) {
+        current = (current + parseInt(n) ) %questions.length;
+        log("Új kérdés következik: " + (current +1)+".");
+        io.emit("newquestion", questions[current][0] + " (" + questions[current][1] + " pont)");
+        timeback = 0;
+        for(var i=0; i<Object.keys(scores).length; i++){
+            scores[Object.keys(scores)[i]].tips = [];
+        }
+        refreshAdmin();
+        }
+    });
+
+    socket.on("disconnect", ()=>{
+        if (typeof socket.user !== "undefined") {
+            var index = sockets.indexOf(socket.user);
+            if (index !== -1) sockets.splice(index, 1); refreshAdmin();
+            log(u + " lecsatlakozott.");
+        }
+    });
+
+    socket.on("newanswer", (data)=> {
+        log("Új válasz: " + JSON.stringify(data));
+        var r;
+        if (current == -1) r = {message: "Várj türelemmel...", class: "text"};
+        else if (timeback === -1) r= {message: "Kifutottál az időből.", class: "text-warning"};
+        else if (scores[u].tips.includes(questions[current][2])) r = {message: "Már megoldottad ezt a feladatot.", class: "text"};
+        else if (scores[u].tips.includes(data.ans)) r = {message: "Ezt már próbáltad! (" + data.ans + ")", class: "text-warning"};
+        else if (data.ans != ""){
+            scores[u].tips.push(data.ans);
+            console.log(data.ans, questions[current][2]);
+            if (data.ans == questions[current][2]){
+                //jó megoldás
+                var p = Math.max(parseInt(questions[current][1])+1-scores[u].tips.length,1);
+                score(u, p);
+                r = {message: "Helyes megoldás! (+"+ p +" pont)", class: "text-success"};
+            }
+            else{
+                //rossz tipp
+                r = {message: "Rossz válasz! (" + data.ans + ")", class: "text-danger"};
+            }
+        }
+        refreshAdmin();
+        socket.emit("reply", r);
+    });
+});
+
+function score(team, points) {
+    scores[team].value = scores[team].value + parseInt(points);
+    log(team + ": " + points, "score.txt")
+    io.emit("score", {team: team, score: scores[team].value});
+    refreshAdmin();
+}
+
+function refreshAdmin(){
+    io.emit("admin:alluser", {sockets: sockets, scores: scores});
+}
+
+app.get("/", (req, res) => {
+    log("Lekérés: " + req.ip);
+    res.render("login", {text: ''});
+});
+
+app.get("/szupertitkos", (req,res)=> {
+    res.render("admin", {questions: questions});
+});
+
+app.get("/stats", (req,res)=> {
+    res.render("stats", {scores: scores});
+});
+
+app.get("/qload", (req,res)=> {
+    qload();
+    res.send("Kérdések újratöltve.");
+});
+
+app.post("/", (req,res) => {
+    var d = req.body.name + "; " + req.body.school;
+    //log("Csatlakozott (" + req.ip+"): " + d);
+    log(d, "users.txt");
+    if (req.body.school == "") req.body.school = "unset";
+    if (current != -1) req.body.q = questions[current][0] + " (" + questions[current][1] + " pont)";
+    res.render("main", req.body);
+});
+
+http.listen(80, "0.0.0.0",() => {
+    log("A szerver elindult.");
+});
